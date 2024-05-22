077f0fa1c3d8ba9ed55cb233501d0de99df915ee
Abdelrahman Ragab Owais <92879288+Abdelrhman1Ragab2@users.noreply.github.com>
2024-05-22 19:07:43 +0300

    Create kubernetes.sh

---
+++ kubernetes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+'kubectl apply -f secret.yaml --validate=false'  
+'kubectl apply -f configmap.yaml --validate=false'
+'kubectl apply -f postgress_serves.yaml --validate=false'
+'kubectl apply -f postgress.yaml --validate=false'  
+'kubectl apply -f backend.yaml --validate=false'  
+'kubectl apply -f backend_service.yaml --validate=false'  
+'kubectl apply -f frontend.yaml --validate=false '  
+'kubectl apply -f frontend_service.yml --validate=false ' 
